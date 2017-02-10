cd42e61c53ccecdd0d4ee446bfd903f84cdf0ac7
Dmitry Chertkov <cheerd@rambler.ru>
2017-02-10 18:42:19 +0300

    Задание 2. Правки 1

--- app/code/local/Cheerd/Test2/Block/Featured.php
+++ app/code/local/Cheerd/Test2/Block/Featured.php
@@ -1,27 +1,15 @@
 <?php
-class Cheerd_Test2_Block_Featured extends Mage_Core_Block_Template
+class Cheerd_Test2_Block_Featured extends Mage_Catalog_Block_Product_Abstract
 {
 	public function getFeaturedProducts()
 	{
 		$storeId = Mage::app()->getStore()->getId();
-		$featuredProducts = Mage::getResourceModel('reports/product_collection')
+		$featuredProducts = Mage::getResourceModel('catalog/product_collection')
 		->addAttributeToSelect('*')
 		->setStoreId($storeId)
-		->addStoreFilter($storeId)
 		->addAttributeToFilter('is_featured', 1)
-		->addAttributeToFilter('status', 1)
+		->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
 		->setPageSize(4);
 		return $featuredProducts;
-	}
-	
-	public function getPrice($product)
-	{
-		$baseCurrencyCode = Mage::app()->getStore()->getBaseCurrencyCode();
-		$currentCurrencyCode = Mage::app()->getStore()->getCurrentCurrencyCode();
-		
-		$price = Mage::helper('directory')->currencyConvert($product->getPrice(), $baseCurrencyCode, $currentCurrencyCode);
-		
-		return Mage::getModel('directory/currency')
-            ->load($currentCurrencyCode)->format($price);
 	}
 }
